373f8ba37c6f03551284a974ecee5a904b6d4936
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2022-04-17 13:56:22 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context context = new Context();
+        DbSet<Category> _object;
+
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            context.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            context.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(Category p)
+        {
+            context.SaveChanges();
+        }
+    }
+}
